3528bdfd6a903a7ed38a62e86161eae5f57eb53a
auvy <datruegvoltage@gmail.com>
2022-11-13 17:18:56 +0200

    prototypes

---
+++ lab1/forward.py
@@ -0,0 +1,8 @@
+class Forward:
+  dict = {}
+  
+  def append_entry(self, document, keywords):
+    return False
+        
+  def search(self, keyword):
+    return False
---
+++ lab1/inverted.py
@@ -0,0 +1,8 @@
+class Inverted:
+  dict = {}
+        
+  def append_entry(self, document, keywords):
+    return False
+  
+  def search(self, keyword):
+    return False
